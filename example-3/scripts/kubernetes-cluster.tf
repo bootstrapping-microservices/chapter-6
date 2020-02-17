b31f7d690bf5abb5276166ea88ab0df0bcf286cc
Ashley Davis <ashley@codecapers.com.au>
2020-02-18 08:59:07 +1000

    Formatting.

--- example-3/scripts/kubernetes-cluster.tf
+++ example-3/scripts/kubernetes-cluster.tf
@@ -33,23 +33,23 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 }
 
 output "cluster_client_key" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].client_key
 }
 
 output "cluster_client_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate
 }
 
 output "cluster_cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
 }
 
 output "cluster_cluster_username" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.username
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].username
 }
 
 output "cluster_cluster_password" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.password
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].password
 }
 
 output "cluster_kube_config" {
@@ -57,6 +57,6 @@ output "cluster_kube_config" {
 }
 
 output "cluster_host" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  value = azurerm_kubernetes_cluster.cluster.kube_config[0].host
 }
 
